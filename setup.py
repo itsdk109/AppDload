95572de9d7a53367b7c0bb6a5b670ceab172ff0f
itsdk109 <dkjob109@gmail.com>
2023-11-09 08:04:50 +0530

    No updates

--- setup.py
+++ setup.py
@@ -3,20 +3,26 @@ import requests
 from google_play_scraper import app
 import os
 
-
+# Define the main function
 def main():
+    # Get the system's username
     system_username = os.getlogin()
-    print_banner()
-    print("\U0001F449 Please type or paste the package name of the app")
-    package_name = input("you want to download: ")
+    # Print the banner
+     # Get the package name from user input
+    print(f"==> Mr. {system_username}! Please type or paste the package name of the app")
+    package_name = input("you want to download (i.e: com.example.myaaps): ")
     print(" ")
+    # Get app info using the provided package name
     app_info = get_app_info(package_name)
     
+    # Check if app info is found
     if app_info:
+        # Download and save the APK
         download_and_save_apk(app_info, system_username)
     else:
         print(f"App with package name '{package_name}' not found on Google Play Store.")
 
+# Define a function to print the banner
 def print_banner():
     banner = '''
 ========================================================================                                 
@@ -25,18 +31,19 @@ def print_banner():
 =       ###  ###  # #       #  # # # #  #                              =
 =       # #  # #  # #       #  # # # #  #                              =
 =       # #  # #  ##        #  ### ###  #####                          =
-=     xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx                         =
-=     x        Android APPs Downlaoder       x                         =
-=     xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx                         =
+=     xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx                 =
+=     x        Android APPs Downloader       x                         =
+=     xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx                 =
 =                                                                      =
 =   Written by Learn Tech With Deepak                                  =
 =   Youtube Channel: @learntechwithdeepak                              =
-=   YouTube: https://www.youtube.com/channel/UCq6THg2VnvaKWnOPF7FGmLw  =
+=   Link: https://www.youtube.com/channel/UCq6THg2VnvaKWnOPF7FGmLw     =
 ========================================================================   
     '''
     print(banner)
     print(" ")
 
+# Define a function to get app info based on package name
 def get_app_info(package_name):
     try:
         app_info = app(
@@ -49,6 +56,7 @@ def get_app_info(package_name):
         print(f"Error: {str(e)}")
         return None
 
+# Define a function to download and save the APK
 def download_and_save_apk(app_info, system_username):
     app_url = app_info.get('url')
     app_name = app_info.get('title', 'UnknownApp')
@@ -61,12 +69,15 @@ def download_and_save_apk(app_info, system_username):
     if response.status_code == 200:
         with open(f'{app_name}.apk', 'wb') as apk_file:
             apk_file.write(response.content)
-            print(f"\U000F60A Congratulations! Mr. {system_username} ):")
-            print(f"'{app_name}' is Downloaded Successfully.....")
             print(" ")
-            print("Thanks You! Mr. {system_username}")
+            print(f"--> Congratulations! Mr. {system_username}, Your '{app_name}' is Downloaded Successfully.....")
+            print(" ")
+            print(f"Thanks for using it, Mr. {system_username}")
     else:
         print("Failed to download APK....")
 
+# Check if the script is being run as the main program
 if __name__ == "__main__":
+    print(" ")
     main()
+    
